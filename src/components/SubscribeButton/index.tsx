4401331c85196764b8da1a90ac110930c1fc8335
Gustavoctt <gustavocarrertartare@gmail.com>
2022-06-02 18:27:11 -0300

    Created function subscribe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,4 @@
+import { signIn, useSession } from 'next-auth/react';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps{
@@ -5,10 +6,22 @@ interface SubscribeButtonProps{
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps){
+  const { data: session } = useSession();
+
+  function handleSubscribeButton(){
+    if(!session){
+      signIn('github');
+      return;
+    }
+
+    
+  }
+  
   return(
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribeButton}
     >
       Subscribe now
     </button>
